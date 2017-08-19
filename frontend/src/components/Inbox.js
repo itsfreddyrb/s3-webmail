af0922d39a8d9655f9e9e9dc186ca7538fd0d315
zachary hill <zhillb@gmail.com>
2017-08-18 21:39:48 -0500

    displaying messages data in inbox app. ejected from create-react-app

--- frontend/src/components/Inbox.js
+++ frontend/src/components/Inbox.js
@@ -2,16 +2,27 @@ import React from 'react';
 import '../styles/Inbox.css';
 
 const Inbox = (props) => {
-    console.log('props from inbox: ', props);
+    console.log(props);
+    const messageTextLength = 20;
     const { messages } = props.messages;
     return (
         <div className="inbox">
+            <h1>Inbox</h1>
+            <div className="headers">
+                <div className="subject">subject</div>
+                <div className="from">from</div>
+                <div className="text">message</div>
+                <div className="date">date</div>
+            </div>
             {messages.map((msg) => {
                 return (
-                    <div className="msg" key={msg._id}>
-                        <span>{msg.subject}</span>
-                        --
-                        <span>{msg.from}</span>
+                    <div className="message" key={msg._id}>
+                        <div className="subject">{msg.subject}</div>
+                        <div className="from">{msg.from}</div>
+                        <div className="text">
+                            {msg.text.substring(0, messageTextLength)}...
+                        </div>
+                        <div className="date">{msg.date}</div>
                         <br />
                     </div>
                 )
