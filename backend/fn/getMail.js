93a5dc27eb042c9b2bc0f31f1aa1a2ff7e133ad9
zachary hill <zhillb@gmail.com>
2017-08-20 20:24:26 -0500

    added functionality to return newest messages first in the messages api call

--- backend/fn/getMail.js
+++ backend/fn/getMail.js
@@ -2,7 +2,8 @@ const { Mail } = require('../config/MailModel');
 
 module.exports = () => {
     return new Promise((fulfill, reject) => {
-        Mail.find({})
+        // return messages by date descending
+        Mail.find({}, null, {sort: {"_id": -1}})
         .then((messages) => {
             fulfill(messages);
         })
